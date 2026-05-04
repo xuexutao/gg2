5dd64a8beee275c16258d5b25077545f43ba954d
xuexutao <xuexutao@bytedance.com>
2026-05-04 23:28:08 +0800

    bug fix eval demo

--- render_lerf_mask.py
+++ render_lerf_mask.py
@@ -3,7 +3,7 @@
 # All rights reserved.
 #
 # ------------------------------------------------------------------------
-# Modified from codes in Gaussian-Splatting 
+# Modified from codes in Gaussian-Splatting
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 
 import torch
@@ -21,37 +21,94 @@ import numpy as np
 from PIL import Image
 import cv2
 
-from ext.grounded_sam import (grouned_sam_output, load_model_hf, load_model_local, resolve_local_groundingdino_paths, select_obj_ioa)
+from ext.grounded_sam import (
+    grouned_sam_output,
+    load_model_hf,
+    load_model_local,
+    resolve_local_groundingdino_paths,
+    select_obj_ioa,
+)
 
 from segment_anything import sam_model_registry, SamPredictor
 
 from render import feature_to_rgb, visualize_obj
 
 
-def render_set(model_path, name, iteration, views, gaussians, pipeline, background, classifier, groundingdino_model, sam_predictor, TEXT_PROMPT, threshold=0.2):
-    render_path = os.path.join(model_path, name, "ours_{}_text".format(iteration), "renders")
+def render_set(
+    model_path,
+    name,
+    iteration,
+    views,
+    gaussians,
+    pipeline,
+    background,
+    classifier,
+    groundingdino_model,
+    sam_predictor,
+    TEXT_PROMPT,
+    threshold=0.2,
+    debug_prompt_selection=False,
+):
+    render_path = os.path.join(
+        model_path, name, "ours_{}_text".format(iteration), "renders"
+    )
     gts_path = os.path.join(model_path, name, "ours_{}_text".format(iteration), "gt")
-    colormask_path = os.path.join(model_path, name, "ours_{}_text".format(iteration), "objects_feature16")
-    pred_obj_path = os.path.join(model_path, name, "ours_{}_text".format(iteration), "test_mask")
+    colormask_path = os.path.join(
+        model_path, name, "ours_{}_text".format(iteration), "objects_feature16"
+    )
+    pred_obj_path = os.path.join(
+        model_path, name, "ours_{}_text".format(iteration), "test_mask"
+    )
+    debug_path = os.path.join(
+        model_path, name, "ours_{}_text".format(iteration), "debug_prompt_selection"
+    )
     makedirs(render_path, exist_ok=True)
     makedirs(gts_path, exist_ok=True)
     makedirs(colormask_path, exist_ok=True)
     makedirs(pred_obj_path, exist_ok=True)
+    if debug_prompt_selection:
+        makedirs(debug_path, exist_ok=True)
 
     # Use Grounded-SAM on the first frame
     results0 = render(views[0], gaussians, pipeline, background)
     rendering0 = results0["render"]
     rendering_obj0 = results0["render_object"]
     logits = classifier(rendering_obj0)
-    pred_obj = torch.argmax(logits,dim=0)
+    pred_obj = torch.argmax(logits, dim=0)
 
-    image = (rendering0.permute(1,2,0) * 255).cpu().numpy().astype('uint8')
-    text_mask, annotated_frame_with_mask = grouned_sam_output(groundingdino_model, sam_predictor, TEXT_PROMPT, image)
-    Image.fromarray(annotated_frame_with_mask).save(os.path.join(render_path[:-8],'grounded-sam---'+TEXT_PROMPT+'.png'))
+    image = (rendering0.permute(1, 2, 0) * 255).cpu().numpy().astype("uint8")
+    text_mask, annotated_frame_with_mask = grouned_sam_output(
+        groundingdino_model, sam_predictor, TEXT_PROMPT, image
+    )
+    Image.fromarray(annotated_frame_with_mask).save(
+        os.path.join(render_path[:-8], "grounded-sam---" + TEXT_PROMPT + ".png")
+    )
     selected_obj_ids = select_obj_ioa(pred_obj, text_mask)
 
+    if debug_prompt_selection:
+        unique_ids = pred_obj.unique()
+        debug_rows = []
+        text_mask_u8 = text_mask.to(torch.uint8)
+        for class_id in unique_ids:
+            class_mask = (pred_obj == class_id).to(torch.uint8)
+            class_area = int(class_mask.sum().item())
+            inter = int((class_mask * text_mask_u8).sum().item())
+            ioa = float(inter / class_area) if class_area > 0 else 0.0
+            debug_rows.append((int(class_id.item()), ioa, class_area, inter))
+        debug_rows.sort(key=lambda x: x[1], reverse=True)
+        debug_file = os.path.join(debug_path, TEXT_PROMPT + ".txt")
+        with open(debug_file, "w") as f:
+            f.write(f"prompt={TEXT_PROMPT}\n")
+            f.write(f"selected_obj_ids={[int(x.item()) for x in selected_obj_ids]}\n")
+            f.write(f"text_mask_pixels={int(text_mask_u8.sum().item())}\n")
+            f.write("top_ioa_classes:\n")
+            for class_id, ioa, class_area, inter in debug_rows[:20]:
+                f.write(
+                    f"  class_id={class_id} ioa={ioa:.6f} class_area={class_area} intersection={inter}\n"
+                )
+
     for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
-        pred_obj_img_path = os.path.join(pred_obj_path,str(idx))
+        pred_obj_img_path = os.path.join(pred_obj_path, str(idx))
         makedirs(pred_obj_img_path, exist_ok=True)
 
         results = render(view, gaussians, pipeline, background)
@@ -60,41 +117,68 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
         logits = classifier(rendering_obj)
 
         if len(selected_obj_ids) > 0:
-            prob = torch.softmax(logits,dim=0)
+            prob = torch.softmax(logits, dim=0)
 
             pred_obj_mask = prob[selected_obj_ids, :, :] > threshold
             pred_obj_mask = pred_obj_mask.any(dim=0)
-            pred_obj_mask = (pred_obj_mask.squeeze().cpu().numpy() * 255).astype(np.uint8)
+            pred_obj_mask = (pred_obj_mask.squeeze().cpu().numpy() * 255).astype(
+                np.uint8
+            )
         else:
-            pred_obj_mask = torch.zeros_like(view.objects, dtype=torch.uint8).cpu().numpy()
+            pred_obj_mask = (
+                torch.zeros_like(view.objects, dtype=torch.uint8).cpu().numpy()
+            )
 
         gt_objects = view.objects
         gt_rgb_mask = visualize_obj(gt_objects.cpu().numpy().astype(np.uint8))
 
         rgb_mask = feature_to_rgb(rendering_obj)
-        Image.fromarray(rgb_mask).save(os.path.join(colormask_path, '{0:05d}'.format(idx) + ".png"))
-        Image.fromarray(pred_obj_mask).save(os.path.join(pred_obj_img_path, TEXT_PROMPT + ".png"))
+        Image.fromarray(rgb_mask).save(
+            os.path.join(colormask_path, "{0:05d}".format(idx) + ".png")
+        )
+        Image.fromarray(pred_obj_mask).save(
+            os.path.join(pred_obj_img_path, TEXT_PROMPT + ".png")
+        )
         print(os.path.join(pred_obj_img_path, TEXT_PROMPT + ".png"))
         gt = view.original_image[0:3, :, :]
-        torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
-        torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
+        torchvision.utils.save_image(
+            rendering, os.path.join(render_path, "{0:05d}".format(idx) + ".png")
+        )
+        torchvision.utils.save_image(
+            gt, os.path.join(gts_path, "{0:05d}".format(idx) + ".png")
+        )
 
 
-
-def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool):
+def render_sets(
+    dataset: ModelParams,
+    iteration: int,
+    pipeline: PipelineParams,
+    skip_train: bool,
+    skip_test: bool,
+    debug_prompt_selection: bool,
+):
     with torch.no_grad():
         dataset.eval = True
         gaussians = GaussianModel(dataset.sh_degree)
         scene = Scene(dataset, gaussians, load_iteration=iteration, shuffle=False)
-        
+
         num_classes = dataset.num_classes
-        print("Num classes: ",num_classes)
+        print("Num classes: ", num_classes)
 
         classifier = torch.nn.Conv2d(gaussians.num_objects, num_classes, kernel_size=1)
         classifier.cuda()
-        classifier.load_state_dict(torch.load(os.path.join(dataset.model_path,"point_cloud","iteration_"+str(scene.loaded_iter),"classifier.pth")))
+        classifier.load_state_dict(
+            torch.load(
+                os.path.join(
+                    dataset.model_path,
+                    "point_cloud",
+                    "iteration_" + str(scene.loaded_iter),
+                    "classifier.pth",
+                )
+            )
+        )
 
-        bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
+        bg_color = [1, 1, 1] if dataset.white_background else [0, 0, 0]
         background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
 
         # grounding-dino
@@ -105,13 +189,19 @@ def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParam
         # Prefer local checkpoints in Tracking-Anything-with-DEVA/saves/.
         # Fallback to HuggingFace if missing.
         local_ckpt_candidates = [
-            os.path.join("Tracking-Anything-with-DEVA", "saves", "groundingdino_swint_ogc.pth"),
+            os.path.join(
+                "Tracking-Anything-with-DEVA", "saves", "groundingdino_swint_ogc.pth"
+            ),
         ]
         local_cfg_candidates = [
-            os.path.join("Tracking-Anything-with-DEVA", "saves", "GroundingDINO_SwinT_OGC.py"),
+            os.path.join(
+                "Tracking-Anything-with-DEVA", "saves", "GroundingDINO_SwinT_OGC.py"
+            ),
         ]
 
-        local_ckpt, local_cfg = resolve_local_groundingdino_paths(local_ckpt_candidates, local_cfg_candidates)
+        local_ckpt, local_cfg = resolve_local_groundingdino_paths(
+            local_ckpt_candidates, local_cfg_candidates
+        )
         if local_ckpt and local_cfg:
             print(f"Using local GroundingDINO: ckpt={local_ckpt}, cfg={local_cfg}")
             groundingdino_model = load_model_local(local_cfg, local_ckpt)
@@ -120,41 +210,66 @@ def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParam
             ckpt_filenmae = "groundingdino_swinb_cogcoor.pth"
             ckpt_config_filename = "GroundingDINO_SwinB.cfg.py"
             print("Local GroundingDINO not found, fallback to HuggingFace download...")
-            groundingdino_model = load_model_hf(ckpt_repo_id, ckpt_filenmae, ckpt_config_filename)
+            groundingdino_model = load_model_hf(
+                ckpt_repo_id, ckpt_filenmae, ckpt_config_filename
+            )
         print("Grounding DINO model loaded.")
-
 
         # sam-hq
         print("Loading SAM-HQ model...")
-        sam_checkpoint = 'Tracking-Anything-with-DEVA/saves/sam_vit_h_4b8939.pth'
+        sam_checkpoint = "Tracking-Anything-with-DEVA/saves/sam_vit_h_4b8939.pth"
         sam = sam_model_registry["vit_h"](checkpoint=sam_checkpoint)
-        sam.to(device='cuda')
+        sam.to(device="cuda")
         sam_predictor = SamPredictor(sam)
         print("SAM-HQ model loaded.")
 
         # Text prompt
-        if 'figurines' in dataset.model_path:
+        if "figurines" in dataset.model_path:
             positive_input = "green apple;green toy chair;old camera;porcelain hand;red apple;red toy chair;rubber duck with red hat"
-        elif 'ramen' in dataset.model_path:
+        elif "ramen" in dataset.model_path:
             positive_input = "chopsticks;egg;glass of water;pork belly;wavy noodles in bowl;yellow bowl"
-        elif 'teatime' in dataset.model_path:
+        elif "teatime" in dataset.model_path:
             positive_input = "apple;bag of cookies;coffee mug;cookies on a plate;paper napkin;plate;sheep;spoon handle;stuffed bear;tea in a glass"
-        elif 'room' in dataset.model_path:
+        elif "room" in dataset.model_path:
             positive_input = "sofa;TV;keyboard;mouse;drinking glass;armchair"
         else:
-            raise NotImplementedError   # You can provide your text prompt here
-        
+            raise NotImplementedError  # You can provide your text prompt here
+
         positives = positive_input.split(";")
         print("Text prompts:    ", positives)
 
         for TEXT_PROMPT in positives:
             if not skip_train:
-                render_set(dataset.model_path, "train", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background, classifier, groundingdino_model, sam_predictor, TEXT_PROMPT)
+                render_set(
+                    dataset.model_path,
+                    "train",
+                    scene.loaded_iter,
+                    scene.getTrainCameras(),
+                    gaussians,
+                    pipeline,
+                    background,
+                    classifier,
+                    groundingdino_model,
+                    sam_predictor,
+                    TEXT_PROMPT,
+                    debug_prompt_selection=debug_prompt_selection,
+                )
             if not skip_test:
-                render_set(dataset.model_path, "test", scene.loaded_iter, scene.getTestCameras(), gaussians, pipeline, background, classifier, groundingdino_model, sam_predictor, TEXT_PROMPT)
+                render_set(
+                    dataset.model_path,
+                    "test",
+                    scene.loaded_iter,
+                    scene.getTestCameras(),
+                    gaussians,
+                    pipeline,
+                    background,
+                    classifier,
+                    groundingdino_model,
+                    sam_predictor,
+                    TEXT_PROMPT,
+                    debug_prompt_selection=debug_prompt_selection,
+                )
 
-
-             
 
 if __name__ == "__main__":
     # Set up command line argument parser
@@ -164,6 +279,7 @@ if __name__ == "__main__":
     parser.add_argument("--iteration", default=-1, type=int)
     parser.add_argument("--skip_train", action="store_true")
     parser.add_argument("--skip_test", action="store_true")
+    parser.add_argument("--debug_prompt_selection", action="store_true")
     parser.add_argument("--quiet", action="store_true")
     args = get_combined_args(parser)
     print("Rendering " + args.model_path)
@@ -171,4 +287,11 @@ if __name__ == "__main__":
     # Initialize system state (RNG)
     safe_state(args.quiet)
 
-    render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test)
+    render_sets(
+        model.extract(args),
+        args.iteration,
+        pipeline.extract(args),
+        args.skip_train,
+        args.skip_test,
+        args.debug_prompt_selection,
+    )
